4ae7d121719d93fa3261d84262e993fc8deaa712
Nameless <dev@name1ess.ga>
2022-06-12 18:20:58 +0100

    Create RocketMan.lua

---
+++ RocketMan.lua
@@ -0,0 +1,77 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = nil -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = 65 -- delay overides the delay
+
+shared.scr = [[
+[60et] e t e t e t
+[Q29e] t y
+[60et] t e t e t y t y
+[Q29t] u [3wtu]
+[4qet] e e t t u [30tuo] s
+[29ety] u y t y e w [et] w
+[7ryio] [yio] [7i] y r o i y
+[7ryoa] [yoa] [7ra] o y d [5w]
+[60et] e t e t e t e
+[Q29t] y y u
+[60et] e t e t y y
+[Q29t] [3wtu]
+[4qet] e t u o [30wtu] s
+[29et] u y t u y u
+[7ryio] [9ryio]
+[qryio] [wryio]
+[80wt] w w t t u u [wti] u y u t
+[4qe] e t y i u t t y u t
+[80w] w t y i u y t y u t
+[4qtip] s y t y t [80w] t e
+[Q29eu] y [ty] u y [30wru]
+[4qeti] u i u i u i u i u [tuo]
+8 u t [8tio] [8tuo]
+[4qeti] e t [eti] [eti] [4q] [eto] [eto] [eti]
+[80wt] w w t t u u [wti] u y u t
+[4qe] e t y i u t t y u t
+[80w] w t y i u y t y u t
+[4qtip] s y t y t [80w] t e
+[Q29eu] y [ty] u y [30wru]
+[4qeti] u i u i u i u i u [tuo]
+8 u t [8tio] [8tuo]
+[4qeti] e t [eti] [eti] [4q] [eto] [eto] [eti]
+[60et] e [6t] e t 6 e t 6 e
+[Q29et] y y [29] u [29] [29]
+[60et] 6 e t 6 e t 6 y [ty]
+[Q29ety] [29] [29] [29] [30wrt]
+[4et] e e [4t] u o [30tu] s [30] u y t
+[29] [ety] [29] [29] [29u] t y
+[7yio] [7yio] [7i] y r o 7 i y
+[7yoa] [7yoa] [7ra] o y d [5w]
+[60et] e t 6 e t e 6 6 u t t
+[Q29ey] y [29] [29] [29]
+[60et] e t [6t] e t 6 e [6t] y y
+[Q29et] [29] [29] e [29t] [30wtu]
+[4qetip] [4q] [30tuo] [30] s
+[29etu] u y t [29] [29] e [29u] t y
+[7ryio] [9ryio]
+[qryio] [wryio]
+[80wt] w w t t u [8u] [wti] u y 8 u t
+[4qe] e t y [4eti] u t t [4y] u t
+[80w] w t y [8wti] u y t [8y] u t
+[4tip] s y t y [4t] [30w] t [3e]
+[Q2eu] y [ty] u y [Q2ey] [30wru]
+[4eti] u i u [4eti] u i u [4i] u [tuo]
+8 u t 8 [tio] 8 [tuo]
+[4qeti] e t [eti] [qeti] [eto] [eto] [4q] [eti]
+[80wt] w w t t u [8u] [wti] u y 8 u t
+[4qe] e t y [4eti] u t t [4y] u t
+[80w] w t y [8wti] u y t [8y] u t
+[4tip] s y t y [4t] [30w] t [3e]
+[Q2eu] y [ty] u y 2 [30wru]
+[4eti] u i u [4eti] u i u [4i] u [tuo]
+8 u t 8 [tio] 8 [tuo]
+[4qet] e [4t] [eti] u i u [4i] u [tuo]
+8 u t 8 [tio] 8 [tuo]
+[4eti] e t i u i u i u o
+8 u t [tio] [tuo]
+[qips] p s g f g f g f s [fh]
+t f s
+]]
