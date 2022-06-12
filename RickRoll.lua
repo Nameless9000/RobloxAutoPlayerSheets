1d92ea3b82ee51c10de54dd4a1296aef790e5aca
Nameless <dev@name1ess.ga>
2022-06-12 18:25:32 +0100

    Create RickRoll.lua

---
+++ RickRoll.lua
@@ -0,0 +1,20 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 39 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- delay overides the delay
+
+shared.scr = [[
+[tuo] [oh] [pj] [oh] |
+tyiy[yip]|[yip]|[8tuo] |
+tyiy[6etuo]|
+[etuo]|
+[9eyi] uy|tyiy [5wyo]|
+[oh] [8tu] yt|t [9eyo] |
+[9eyi] |tyiy[yip]|
+[yip]|[8tuo] |
+tyiy[6ets]|[6wu]|
+[9eti] uy tyiy [5wyo]|
+o [8tu] yt|t [6eto]|
+[9eyi]
+]]
