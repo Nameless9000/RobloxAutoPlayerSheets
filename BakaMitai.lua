e3c3aec4cda2e7079bb561f7dadc7490b76fa4ef
Nameless <dev@name1ess.ga>
2022-06-12 18:15:09 +0100

    Update BakaMitai.lua

--- BakaMitai.lua
+++ BakaMitai.lua
@@ -1,10 +1,8 @@
--- Sheet for Baka Mitai
-
-shared.stop = false -- stops the player at any time if true
+shared.stop = true -- stops the player at any time if true
 -- CONFIG:
-shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.ftime = 60+52 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
-shared.tempo = nil -- tempo overides the delay
+shared.tempo = nil -- delay overides the delay
 
 shared.scr = [[
 uiopiopaopasdfgh[4sj]-qt [upsf][ig]qti[pfjlx]-
