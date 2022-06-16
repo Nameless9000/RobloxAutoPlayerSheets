419f9e3926ee43b9548f03d1df26772f2d703869
Nameless <dev@name1ess.ga>
2022-06-16 18:08:55 +0100

    Update JocelynFlores.lua

--- JocelynFlores.lua
+++ JocelynFlores.lua
@@ -2,74 +2,45 @@
 
 shared.stop = false -- stops the player at any time if true
 -- CONFIG:
-shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.ftime = 2*60 + 12 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
 
 shared.scr = [[
-[Eyipd] f g [dg]
-[yipsl] j p s
-[fl] [tuo] f u
-[etuo] o e
-[ud] s [Eyipd] f i g
-[yipsjx] g p
-[sj] [tuo] [fl] u o
-[etuos] u [pj] h
-[eg] f [^id] [if] q [Eig]
-[9dgl] [dgj] e i 9
-[sfl] 8 [sf] w t 8
-6 0 e t u
-[6d] s [5id] 9 [wif] E [yig]
-[9gj] [gjl] e i 9
-[sj] 8 [sfl] w t 8
-[6psf] 0 [ps] [tj] h
-[6g] f [^id] q [^Eif] y [ig]
-[9dgl] [edgj] [yi] 9 e
-[sfl] 8 [sf] w t 8
-6 0 e 0 t 6
-d s [5id] 9 [wif] E [yig]
-[9pg] [egl] i 9
-[sj] 8 [sfl] w t 8
-[6psf] 0 [ps] [tj] h
-[6g] f [^2pd] [pf] [^2q] [^EPig] ^
-[2yidgl] [edgj] i 9
-[sf] [18] [sf] w t 8
-[6e] [60] e 0 t 6
-d s [^2pd] [ipf] [^q] [^Eig]
-[29pgj] [egjl] i 9
-[sj] [18] [sf] w t 8
-[6eps] [60] [tpsj] h
-[60g] f [^2pdg] [pf] [^2q] [^EPig] ^
-[2ydgl] [edgj] i 9
-[sf] [18] [sf] w t 8
-[6d] [60s] t 0
-[6d] s [5wipd] [ipf] 9 [wig]
-[29ypgj] [egjl] i 9
-[psj] [18] [sf] w t 8
-[6epsf] [60] [tpsj] h
-[60g] f [^qed] [ps] j h
-g f [9etsf] s s [pf] g
-[pf] 8 w t u o
-6 0 g [tj] j
-[6l] [^dg] q E y i d
-[^Pj] [9h] e y [ipg] y
-9 f g [8sf] w t u o
-6 0 e t [usj] h
-[6g] f [EPyid] [Pd] j h
-g f [yipsf] s s f g
-f [tuo] u o t
-e t g [uj] j
-[el] [Eyipg] i p [Ed]
-j [yipsh] g i p
-y f g [tuof] o u t
-e t u
-o [Ey] [pgz] f [gc]
-[yips] j p [tuo] [fl]
-[etu] e d s [Eyipd] [fx] [gc]
-[yips] l p [jb] [tuo] l
-[fl] [etuo] [ojlb] [hv] [gc] [fx] [Eyidz] [fx] [gc]
-[yipslx] [jl] p [tuo] [fl]
-[etu] e d s [Ewyd] f [gc]
-[yipsj] i p [sfj] [tuo]
-f [etuo]
+[utqpe]|a|s [sp][utohe]||f|
+u o[ha]|[ywr]|a|r|[ywr0]||
+y 0 [rp] o [utqpe]|a t s|[utokfe]||
+s|u|[of]|[ywr]|[ha]|r y [ywro0]||
+r[uf] d [s0] a [tp4]|[ta] 8 [tsq]|[sph6]||
+[spf] 0 t 6 [oha]|5|[oa] 9 w 5 3| 7 0 w r|
+[p3] o [tp2] 6 [ta9] q [tse]|[sf6]||
+[shf] 0 t 6 [of]|5|[oha] 9 w 5 [uoa3]||
+7 [uo][wf] d [s3] a [tp4] 8 [tqa4] e [ts]|[sph6]||
+[spf0] [te] 6 0 [oha]|5|[oa] 9 w 5 3|
+7 0 7 w 3p o [tp2] 6 [ta9] q [tse]|[us6]||
+[sh0]|t 6 [of]|5|[oha] 9 w 5 [uoa3]||
+7 [uo][wf] d [s3] a [up4]|[ua] [84] [tsqi4] 4 [tsphe6]||
+[spf0]|t 6 [oa]|5|[oa] 9 w 5 [30]|
+[73] 0 7 w 3 p [o3] [up4]|[uta] [84] [tsq4]|[usf6]||
+[shf0]|t 6 [of]|5|[oa] 9 w 5 [uo30]||
+[73] [wuof] d [s73] a [usp4]|[ua] [84] [tsqi4] 4 [sphe6]||
+[spf0]|t 6 [oa]|5|[oa] 9 w 5 [p3]||
+[o73]|w 7 [p3] o [utp92]|[uta] 6 [ts9]|[usfe6]||
+[shf0]|t 6 [uof]|5|[oa] 9 w 5 [uoa30]||
+[73] [wuof] d [s73] a [p840]||[uo]fdsa [woa60]|
+o o [ua] s [ua]|5 9 w r y|3||
+7 s [wf] f [h3]|[sp4] 8 q e t p [if4]|
+[d6] 0 e [uts] e|6 as[oa5] 9 w r y|3|
+7 0 w [rof] d [s3] a [tqpie]|
+[pi]f d s a [utoea]|o o a s a|[ywr]| r y w 0||
+w s [rf] f [h0]|[utsqe]| t u [qp] f|[utoed]|
+s t u e as[ywra]| y r w 0||
+w|r|y| [qe] [usj] a [sl] [utoe]|
+f u|[ywr] [ha]|[wr0]| 0
+po[utqpe] [ka] [sl] [utoe]|
+h u [xf] [ywr] h|[ha] [ywr0]|
+[yxhf][zd][sl][ka][tqpje] [ka] [sl]
+[utokhe]|[hf] u|[ywr] [ha]|
+[wr0]| 0 po[qpe9] a [sl]
+[utofe]|t u [ofa] [ywr]| a [ywr0]
 ]]
