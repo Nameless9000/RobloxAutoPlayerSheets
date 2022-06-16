22f61983d9c0ad15c8c55f5293860013d054cc72
Nameless <dev@name1ess.ga>
2022-06-16 18:09:51 +0100

    Update ChangesXXX.lua

--- ChangesXXX.lua
+++ ChangesXXX.lua
@@ -2,7 +2,7 @@
 
 shared.stop = false -- stops the player at any time if true
 -- CONFIG:
-shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.ftime = 1*60 + 58 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
 
